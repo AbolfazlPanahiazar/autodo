b3f20fbe88c2b7d446847e26d6adefe08820b5bc
Abolfazl Panahiazar <abolfazl.panahiazar@rambody.me>
2022-07-17 15:17:37 +0430

    Add crud logic

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,21 @@ import { Todo } from "./todo";
 
 export class App {
   public message: string = "Hello World!";
-  public todo: Todo;
+  public todoList: Todo[] = [];
+  public newItem: string = "";
 
   constructor() {
-    this.todo = new Todo("Clean my room.");
+    this.todoList.push(new Todo("Clean my room"));
+    this.todoList.push(new Todo("Take a shower"));
+    this.todoList.push(new Todo("Aurelia task"));
+  }
+
+  addTodo() {
+    this.todoList.push(new Todo(this.newItem));
+    this.newItem = "";
+  }
+
+  remoteTodo(todo: Todo) {
+    this.todoList.splice(this.todoList.indexOf(todo), 1);
   }
 }
